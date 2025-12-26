0162eb078975c01fc96471b672309285d1d0e64c
bdzhdmh <BDZHDMH@GMAIL.COM>
2025-12-26 20:38:57 +0330

    Add project files.

---
+++ peoject 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace consoleapp2
+{
+    class PROGRAM
+    {
+        static void Main(String[] ARGS)
+        {
+            int nuimber1 = 6;
+            int nuimber2 = 7;
+            int risult = nuimber1 + nuimber2;
+            Console.WriteLine(nuimber2);
+            Console.WriteLine(risult);
+            risult = nuimber1;
+            Console.WriteLine(risult);
+            risult = nuimber2 * 18;
+            Console.WriteLine(risult);
+            int n1 = 100;
+            const int sabet = 10;
+            Console.WriteLine(sabet);
+            Console.WriteLine(sabet * 10);
+            int n2 = sabet * 5;
+            Console.WriteLine(n2);
+        }
+    }
+    }
